dfe5b0db4b302d7fb94ffc8a5bd2cbca220e9264
anujrawat14 <anujrawatff@gmail.com>
2024-11-04 04:17:04 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) we are using node js not browser
+console.log(3
+    +3
+)  //code readability should be high
+
+
+
+                         //DATA TYPES N JS
+
+
+let name="anuj"   //String=""
+
+let age=20        //number =>  2 to power 53
+                  //bigint
+
+let IsLoggedIn=false   //boolean => true/false
+
+let state;         //undefined=>
+
+let RemainingPurse=null;   //null=> stand alone values
+
+// symbol => unique
+//object
+
+//type of keyword is used to knoe the data type
+
+console.log(typeof(null)); // output is object
+console.log(typeof(undefined)); // output is undefined
+ 
