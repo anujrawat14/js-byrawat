050ea46342a472e6b5b59d33e0403a4276344579
anujrawat14 <anujrawatff@gmail.com>
2024-11-04 04:07:07 +0000

    variables

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=14453
+let accountEmail="xyz@gmail.com"
+var accountPasswords=1456
+accountCity="jaipur"
+
+//accountId=2 //not alllowed
+accountEmail="hello@gmaial.com"
+accountPasswords=22222
+accountCity="delhi"
+let AcccountState;
+console.log(accountId)
+/*
+ DONT USE VAR KEYWORD BEACUSE OF ISSUES BLOCK SCOPE AND FUNCTIONAL SCOPE
+ */
+console.table([accountEmail,accountId,accountPasswords,accountCity])
+console.log(AcccountState);
