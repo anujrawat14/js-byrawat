b69fe1705b2adc54b03f71646667ba60486d43f9
anujrawat14 <anujrawatff@gmail.com>
2024-11-05 04:11:33 +0000

    CONVERSION OPERATIONS

--- 01-basics/03_conversionoperations.js
+++ 01-basics/03_conversionoperations.js
@@ -46,3 +46,47 @@ let stringnumb=String(randnum)
 console.log(stringnumb);
 console.log(typeof stringnumb);
 
+//***********************************  OPERATIONS   **********************************/
+
+let value=3
+let negValue=-value
+console.log(negValue);
+
+let str1="hello"
+let str2="rawat"
+console.log(str1+str2)
+
+console.log("1"+2)  //output is 12
+console.log(1+"2")  //output is 12
+console.log(1+3+"2")  //output is 42 number first
+console.log("1"+3+2)  //output is 132 string first
+
+console.log(true) // output is true
+console.log(+true);// output is 1
+console.log(+""); // output is 0
+
+let num1,num2,num3;
+num1=num2=num3=2+2
+
+// let gamecounter=100
+// gamecounter++;
+// console.log(gamecounter)   // 101 output
+
+let gamecounter=100
+++gamecounter;
+console.log(gamecounter)   // 101 output
+
+
+// POSTFIX AAND PREFIX OPERATIONS
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
