9e1b63cd96946920d8faeaf7344948ccd6252163
anujrawat14 <anujrawatff@gmail.com>
2024-11-06 03:53:41 +0000

    data type + memory

--- 01-basics/datatypessummary.js
+++ 01-basics/datatypessummary.js
@@ -16,13 +16,13 @@ const bignumber = 234567892n    // big int data types
 
 const heros = ["shaktiman", "nagraj", "doga"]
 const details = {
-           studentname:"rawat",
-           rollnumber:18
+    studentname: "rawat",
+    rollnumber: 18
 }
-const myfunction= function(){
+const myfunction = function () {
     console.log("hello world");
-    
-} 
+
+}
 
 console.log(typeof details);  //object
 console.log(typeof myfunction); //object function
@@ -30,3 +30,24 @@ console.log(typeof heros);//object
 
 
 
+//*********************************  MEMORY    ********************************* */
+// stack(primitive);heap(non primitive)
+
+let myytname = "rawatjiff"
+
+let anothername = myytname;
+anothername = "anujff";
+
+console.log(anothername);
+
+
+let user1 = {
+    email: "anujrawatff@gamail.com",
+    upiid: "user1@sbi.com"
+}
+let user2 = user1
+
+user2.email = "anuj@gmail.com"
+
+console.log(user1.email);    //output is changed "anuj@gmail.com"
+console.log(user2.email);    //output is "anuj@gmail.com"
