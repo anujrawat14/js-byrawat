dec0f437c607f18200dbb27ac9730d9b3e75a72e
anujrawat14 <anujrawatff@gmail.com>
2024-11-07 05:06:11 +0000

    numberts and maths

---
+++ 01-basics/06_number_maths.js
@@ -0,0 +1,28 @@
+const score=400
+
+const balance=new Number(100)
+console.log(balance);
+
+console.log(typeof balance.toString());
+console.log(balance.toFixed(2));  //output is 100.00
+
+const othernum=123.89996
+console.log(othernum.toPrecision(4));
+
+const hundreds=10000000
+console.log(hundreds.toLocaleString("en-In"));//by default it sets US standard
+
+
+//********************************   MATHS    *********************************/
+
+//MATH IS AN OBJECT
+
+console.log( Math.abs(-5));  //converts negative integer to positive
+console.log(Math.round(4.8));//it is used to round of given number
+console.log(Math.ceil(4.8));//it is used to round of given number
+console.log(Math.(4.8));//it is used to round of given number
+con 
+
+
+
+
